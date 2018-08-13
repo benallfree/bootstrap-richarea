097497694c3c45cdef7736df690951f2ca53395e
Ben Allfree <ben@benallfree.com>
2018-08-13 16:45:52 -0700

    Markdown editor bugfix

--- src/js/editors/MarkdownEditor.js
+++ src/js/editors/MarkdownEditor.js
@@ -1,40 +1,38 @@
-let BaseEditor = require('./BaseEditor');
+let BaseEditor = require("./BaseEditor");
 
-require( 'simplemde/src/css/simplemde.css');
-require( 'codemirror/lib/codemirror.css');
-require( 'codemirror-spell-checker/src/css/spell-checker.css');
-let simplemde = require('simplemde');
-let marked = require('marked');
-let toMarkdown = require('to-markdown');
+require("simplemde/src/css/simplemde.css");
+require("codemirror/lib/codemirror.css");
+require("codemirror-spell-checker/src/css/spell-checker.css");
+let simplemde = require("simplemde");
+let marked = require("marked");
+let toMarkdown = require("to-markdown");
 
 jQuery.fn.visible = function() {
-    return this.css('visibility', 'visible');
+  return this.css("visibility", "visible");
 };
 
 jQuery.fn.invisible = function() {
-    return this.css('visibility', 'hidden');
+  return this.css("visibility", "hidden");
 };
 
 jQuery.fn.visibilityToggle = function() {
-    return this.css('visibility', function(i, visibility) {
-        return (visibility == 'visible') ? 'hidden' : 'visible';
-    });
+  return this.css("visibility", function(i, visibility) {
+    return visibility == "visible" ? "hidden" : "visible";
+  });
 };
 
-class MarkdownEditor extends BaseEditor
-{
-  static initField(field, layout, options)
-  {
-    let assetRoot = options.editors[this.slug].assetRoot || layout.assetRoot
+class MarkdownEditor extends BaseEditor {
+  static initField(field, layout, options) {
+    let assetRoot = options.editors[this.slug].assetRoot || layout.assetRoot;
     field.defaultValue = marked(field.defaultValue);
   }
-  
+
   static getVueData() {
     return {
-      props: ['item', 'fieldName'],
+      props: ["item", "fieldName"],
       data() {
         return {
-          isInitialized: false,
+          isInitialized: false
         };
       },
       template: `
@@ -43,43 +41,41 @@ class MarkdownEditor extends BaseEditor
         </div>
       `,
       methods: {
-        showBsModal () {
+        showBsModal() {
           console.log(`before show ${this.item.layoutId}:${this.fieldName}`);
-          if(this.isInitialized) return;
+          if (this.isInitialized) return;
           this.$editor.invisible();
         },
-        shownBsModal () {
+        shownBsModal() {
           console.log(`on show ${this.item.layoutId}:${this.fieldName}`);
-          if(this.isInitialized) return;
-          this.mde = new simplemde({element: this.$refs.area })
+          if (this.isInitialized) return;
+          this.mde = new simplemde({ element: this.$refs.area });
           this.mde.value(toMarkdown(this.item.data[this.fieldName]));
-          this.mde.codemirror.on('change', ()=>{
+          this.mde.codemirror.on("change", () => {
             this.item.data[this.fieldName] = marked(this.mde.value());
           });
           this.isInitialized = true;
         },
-        hideBsModal () {
-        },              
+        hideBsModal() {}
       },
       mounted() {
         console.log(`mount ${this.item.layoutId}:${this.fieldName}`);
         this.$root = $(this.$el);
-        this.$modal = this.$root.closest('.modal');
-        this.$editor = this.$root.find('.markdown-editor');
-        this.$modal.on('show.bs.modal', this.showBsModal);
-        this.$modal.on('shown.bs.modal', this.shownBsModal);
-        this.$modal.on('hide.bs.modal', this.hideBsModal);
-        
+        this.$modal = this.$root.closest(".modal");
+        this.$editor = this.$root.find(".markdown-editor");
+        this.$modal.on("show.bs.modal", this.showBsModal);
+        this.$modal.on("shown.bs.modal", this.shownBsModal);
+        this.$modal.on("hide.bs.modal", this.hideBsModal);
       },
-      beforeDestroy () {
+      beforeDestroy() {
         console.log(`unmount ${this.item.layoutId}:${this.fieldName}`);
-        this.$modal.off('show.bs.modal', this.showBsModal);
-        this.$modal.off('shown.bs.modal', this.shownBsModal);
-        this.$modal.off('hide.bs.modal', this.hideBsModal);
-      },
+        this.$modal.off("show.bs.modal", this.showBsModal);
+        this.$modal.off("shown.bs.modal", this.shownBsModal);
+        this.$modal.off("hide.bs.modal", this.hideBsModal);
+      }
     };
   }
 }
-MarkdownEditor.slug = 'MarkdownEditor'
+MarkdownEditor.slug = "MarkdownEditor";
 
 module.exports = MarkdownEditor;
